907bf904fb61fdd74db606408401dba9ca649047
Ivanildo Batista da Silva Júnior <juniorw.12@outlook.com>
2025-01-22 00:40:52 -0300

    Add files via upload

---
+++ Comentando_codigos/SQLQuery_Comentando_codigo.sql
@@ -0,0 +1,26 @@
+USE [db_sisfinanceiro]
+GO
+
+/****** Object:  Table [dbo].[tbl_vendas]    Script Date: 14/01/2025 14:52:02 ******/
+
+--Comentando uma código
+-- Usa o "--" ou o "/*  */"
+
+
+/*SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[tbl_vendas](
+	[CODVENDA] [int] NOT NULL,
+	[VENDEDOR] [varchar](50) NOT NULL,
+	[VALORDAVENDA] [float] NULL,
+	[DATAVENDA] [date] NOT NULL
+) ON [PRIMARY]
+GO
+
+*/
+
+USE
